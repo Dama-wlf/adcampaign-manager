036ad6f7368006bec6de1bd229de15d6644b6212
Dama-wlf <damaanil1504@gmail.com>
2026-02-05 07:25:04 +0300

    Modification de modele et server

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/app.js";
+import campaignRoutes from "./routes/campaignRoutes.js";
 
 dotenv.config();
 
@@ -10,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
-
+app.use("/api/campaigns", campaignRoutes);
 
 app.listen(PORT, () => {
     connectDB();
