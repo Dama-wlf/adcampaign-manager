1477b9c305fad2c1f184caee7ace8384897c4e9a
Dama-wlf <damaanil1504@gmail.com>
2026-02-05 14:28:00 +0300

    Modification de Middleware autoCampaign

--- backend/middlewares/autoCampaign.js
+++ backend/middlewares/autoCampaign.js
@@ -1,6 +1,6 @@
 import Campaign from "../models/campaignModel.js";
 
-export const autoFinishCampaign = async (req, res, next) => {
+export const autoCampaign = async (req, res, next) => {
   try {
     const { id } = req.params;
     const campaign = await Campaign.findById(id);
@@ -8,8 +8,12 @@ export const autoFinishCampaign = async (req, res, next) => {
         return res.status(404).json({ error: "Campagne introuvable" });
     }
 
-    if (campaign.endDate < new Date() && campaign.status !== "finished") {
+    if (campaign.endDate < new Date()) {
       campaign.status = "finished";
+      await campaign.save();
+    }
+    if (campaign.startDate <= new Date() && campaign.status === "paused") {
+      campaign.status = "active";
       await campaign.save();
     }
 
